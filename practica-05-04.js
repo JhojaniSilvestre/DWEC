7d62dadd1187270f6a9c84542391cab4e8a2c420
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2021-09-29 09:29:33 +0200

    Update practica-05-04.js

--- practica-05-04.js
+++ practica-05-04.js
@@ -7,8 +7,6 @@ tenemos un número y le vamos a convertir a binario, octal y hexadecimal;
 mostrando los valores en sus respectivas cajas de texto.
 */
 
-/*funcion index of e includes|| es mejor partit del cao true y encontrar los falses*/
-
 window.onload= Conversor;
 function Conversor() {
     document.formulario.convertir.onclick = Convierte;
@@ -19,7 +17,7 @@ function Convierte(){
     document.formulario.octal.value="";
     document.formulario.hexadecimal.value="";
     const valorDecimal = parseInt(document.formulario.decimal.value);
-    const resulBinario = valorDecimal.toString(2); //El toString con base 2 convierte el numBinarioero en cadena en formato binario
+    const resulBinario = valorDecimal.toString(2); 
     const resulOctal = valorDecimal.toString(8);
     const resulHexa = valorDecimal.toString(16);
     document.formulario.binario.value += resulBinario;
@@ -27,16 +25,3 @@ function Convierte(){
     document.formulario.hexadecimal.value += resulHexa;  
 }
 
-/*
-function Convierte(){
-    document.formulario.octal.value="";
-    document.formulario.hexadecimal.value="";
-    const numBinario = parseInt(document.formulario.decimal.value);
-    const resulBinario = numBinario.toString(2); //El toString con base 2 convierte el numBinarioero en cadena en formato binario
-    const numOctal = parseInt(document.formulario.decimal.value);
-    const resulOctal = numBinario.toString(16);
-    const numHexa = parseInt(document.formulario.decimal.value);
-    const resulHexa = numBinario.toString(8);
-    document.formulario.binario.value += resul;  
-}
-*/
